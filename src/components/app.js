f52c3cda800a4143d9bcfd57646bb41aa58bfd99
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-07-07 18:30:48 +0300

    Add registration feature
    - Takes 3 fields back to our backend and redirects upon successful registration to the dashboard.

--- src/components/app.js
+++ src/components/app.js
@@ -2,15 +2,48 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Dashboard from "./Dashboard";
+import { timingSafeEqual } from 'crypto';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    }
+
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(data) {
+    this.setState({
+      loggedInStatus: "LOGGED IN",
+      user: data.user
+    });
+  }
+
   render() {
     return (
       <div className='app'>
         <BrowserRouter>
           <Switch>
-            <Route exact path={"/"} component={Home} />
-            <Route exact path={"/dashboard"} component={Dashboard} />
+            <Route 
+              exact 
+              path={"/"} 
+              render={props => (
+                <Home {...props} 
+                  handleLogin={this.handleLogin}
+                  loggedInStatus={this.state.loggedInStatus} />
+              )}
+            />
+            <Route 
+            exact 
+            path={"/dashboard"} 
+            render={props => (
+              <Dashboard {...props} loggedInStatus={this.state.loggedInStatus} />
+            )}
+            />
           </Switch>
         </BrowserRouter>
       </div>
